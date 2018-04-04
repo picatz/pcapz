b8018ea64aad2ffc69d3079368401591da628cd3
Kent Gruber <kgruber1@emich.edu>
2018-04-04 16:55:28 -0400

    add small packet capturing spec

--- spec/pcapz_spec.rb
+++ spec/pcapz_spec.rb
@@ -2,4 +2,17 @@ RSpec.describe Pcapz do
   it "has a version number" do
     expect(Pcapz::VERSION).not_to be nil
   end
+  
+  it "can capture a packet" do
+    cap = Pcapz.capture.new
+    expect(cap.next_packet).not_to be nil
+  end
+  
+  it "can capture a stream of packets, yielding to a block" do
+    cap = Pcapz.capture.new
+    cap.packets do |packet|
+      expect(packet).not_to be nil
+      break
+    end
+  end
 end
