5b7f4b9efd0dbfbe96b5b48ab7706de560cf5fbb
Kent Gruber <kgruber1@emich.edu>
2018-04-03 23:36:03 -0400

    cleanup capture class login a bit

--- lib/pcapz/capture.rb
+++ lib/pcapz/capture.rb
@@ -1,17 +1,17 @@
 module Pcapz
   class Capture
     def initialize(interface = Interfacez.default)
-      @interface = interface
+      @interface   = interface
       @buffer_size = 0
-      @file = nil
+      @file        = nil
       configure_bpf_dev
     end
 
-    def read_packets(capture = @file)
-      return nil if capture.nil? or capture.closed?
+    def packets
+      return nil if @file.closed?
       if block_given?
         loop do
-          buffer = capture.read_nonblock(@buffer_size)
+          buffer = @file.read_nonblock(@buffer_size)
           while buffer.size > 0
             size, hdrlen = header_decode(buffer)
             pkt = buffer.slice!(0,size)[hdrlen..-1] 
@@ -20,7 +20,7 @@ module Pcapz
         end
       else
         packets = []
-        buffer = capture.read_nonblock(@buffer_size)
+        buffer = @file.read_nonblock(@buffer_size)
         while buffer.size > 0
           size, hdrlen = header_decode(buffer)
           pkt = buffer.slice!(0,size)[hdrlen..-1] 
@@ -30,10 +30,9 @@ module Pcapz
       end
       return true
     rescue EOFError, Errno::EAGAIN
-      #sleep(0.25)
       retry
     rescue IO::WaitReadable
-      IO.select([capture])
+      IO.select([@file])
       retry
     end
 
@@ -42,36 +41,35 @@ module Pcapz
     end
 
     def stop!
+      return nil if stopped?
       @file.close
       stopped?
     end
 
     def stopped?
-      @file.closed? unless @file.nil?
+      @file.closed?
     end
 
     private
 
     def configure_bpf_dev(interface: @interface)
-      file = nil
       Dir.glob('/dev/bpf*') do |item|
         begin
-          file = File.open(item)   
+          @file = File.open(item)   
         rescue Errno::EBUSY
           next
         end
       end
-      unless file.nil?
-        file.ioctl(0x8020426c, [interface].pack("a#{interface.size+1}"))
-        file.ioctl(0x80044270, [1].pack('I'))
-        file.ioctl(0x40044274, [0].pack('N'))
-        buf = [0].pack('i')
-        file.ioctl(0x40044266, buf)
-        @buffer_size = buf.unpack('i')[0]
-        timeout = [5,0].pack('LL')
-        file.ioctl(0x8008426d, timeout)
-      end 
-      return @file = file
+      raise "Unable to start a packet capture on any device" if @file.nil?
+      @file.ioctl(0x8020426c, [interface].pack("a#{interface.size+1}"))
+      @file.ioctl(0x80044270, [1].pack('I'))
+      @file.ioctl(0x40044274, [0].pack('N'))
+      buf = [0].pack('i')
+      @file.ioctl(0x40044266, buf)
+      @buffer_size = buf.unpack('i')[0]
+      timeout = [5,0].pack('LL')
+      @file.ioctl(0x8008426d, timeout)
+      return @file 
     rescue
       @file.close unless @file.nil? or @file.closed?
     end
